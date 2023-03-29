10e2d238fff62fecdf22d0c0a1ee497125f880ca
pinlok <s.morze@ukr.net>
2023-03-29 14:10:49 +0300

    Write the lines that are in both files to same.txt

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ with open('file1.txt', 'r') as file1, open('file2.txt', 'r') as file2, open('sam
     # Read the contents of the two files into lists
     file1_lines = file1.readlines()
     file2_lines = file2.readlines()
+    # Write the lines that are in both files to same.txt
+    same_lines = set(file1_lines) & set(file2_lines)
+    for line in same_lines:
+        same_file.write(line)
